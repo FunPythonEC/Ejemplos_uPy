e2d52d8a0cc9a865601d85a78b6a669aa26b24f8
sasilva1998 <30530787+sasilva1998@users.noreply.github.com>
2019-03-12 16:23:14 -0500

    mejora de prints y errores

--- robustwlan.py
+++ robustwlan.py
@@ -3,16 +3,27 @@ import time
 import _thread    
 def connwifithread(ssid, clave):
     nic = network.WLAN(network.STA_IF)
-    nic.active(True)
-    while True:
-        if not nic.isconnected():
-            nic.connect(ssid, clave)
-            print(nic.isconnected())
-            print(nic.ifconfig())
-        elif nic.isconnected():
-            print(nic.isconnected())
-            print(nic.ifconfig())
-            pass 
-        time.sleep(1)   
+    try:
+        nic.active(True)
+    except Exception as e:
+        print("No se pudo activar WiFi: "+str(e))
+
+    if nic.active():
+        print("WiFi activado")
+        while True:
+            if not nic.isconnected():
+                try:
+                    nic.connect(ssid, clave)
+                except Exception as e:
+                    print(e)
+                time.sleep(3)
+                if nic.isconnected():
+                    print("Se ha conectado a la red")
+                    print(nic.ifconfig())
+            elif nic.isconnected():
+                pass
+
+    else:
+        print("Intentar ejecutar el metodo connectwifi de nuevo")  
 def connectwifi(nombre, clave):
     _thread.start_new_thread(connwifithread(nombre, clave), ())
